922edd9eaa2e869a6fdbb5816f3539287dd9aec5
Kai Volland <volland@terrestris.de>
2023-09-19 15:44:41 +0200

    feat: host material-symbols font

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import './app.css'
+import './assets/style/material-symbols.css';
 import App from './App.svelte'
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.getElementById('app')!
 })
 
 export default app
